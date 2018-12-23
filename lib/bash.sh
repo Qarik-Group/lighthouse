93c21c1ee2e90375185b22f7fbec63162d616b67
norman-abramovitz <norm@starkandwayne.com>
2018-12-22 20:22:26 -0800

    1) document args are optional 2) check args are numeric

--- lib/bash.sh
+++ lib/bash.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# args [major] [minor] [patch]
 need_bash_minimum_version()
 {
     case $# in
@@ -7,12 +8,14 @@ need_bash_minimum_version()
             true
             ;;
         1)
-            ((${BASH_VERSINFO[0]} >= $1))
+            [[ $1 =~ ^[0-9]+$ ]] && ((${BASH_VERSINFO[0]} >= $1))
             ;;
         2)
+            [[ $1 =~ ^[0-9]+$ && $2 =~ ^[0-9]+$ ]] && 
             ((${BASH_VERSINFO[0]} > $1 || (${BASH_VERSINFO[0]} == $1 && ${BASH_VERSINFO[1]} >= $2) ))
             ;;
         3)
+            [[ $1 =~ ^[0-9]+$ && $2 =~ ^[0-9]+$ && $3 =~ ^[0-9]+$ ]] && 
             (( ${BASH_VERSINFO[0]} > $1 ||
               (${BASH_VERSINFO[0]} == $1 && ${BASH_VERSINFO[1]} > $2) ||
               (${BASH_VERSINFO[0]} == $1 && ${BASH_VERSINFO[1]} == $2 && ${BASH_VERSINFO[2]} >= $3) ))
