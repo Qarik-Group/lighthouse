171ca703039e0b31ee77bd9f58290c061fb9f9a4
Kevin Rutten <Kevin@KevinRutten.com>
2019-11-11 08:55:36 -0800

    Fix typos in update

--- templates/tests/cf/feature_flags.sh
+++ templates/tests/cf/feature_flags.sh
@@ -3,7 +3,7 @@
 #lh_test_requires cf
 
 if [[ "" == "${LH_DIRECTORY}" ]] ; then
-    echo "Please run this test through Lighthouse or set the LH_DIRECTORY and BASH_ENV variables"
+    echo "Please run this test through Lighthouse or set the LH_DIRECTORY and BASE_ENV variables"
     exit 1
 fi
 
@@ -23,10 +23,10 @@ then
     validation_data="${base_validation_data}"
 fi
 
-if [[ "" != "${BASH_ENV}" ]] && [[ -e "${BASH_ENV}/${base_validation_data}" ]] ;
+if [[ "" != "${BASE_ENV}" ]] && [[ -e "${BASE_ENV}/${base_validation_data}" ]] ;
 then
-    echo "Found and using ${BASH_ENV}/${base_validation_data}"
-    validation_data="${BASH_ENV}/${base_validation_data}"
+    echo "Found and using ${BASE_ENV}/${base_validation_data}"
+    validation_data="${BASE_ENV}/${base_validation_data}"
 fi
 fab_validate_data()
 {
